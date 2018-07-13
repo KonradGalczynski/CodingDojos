805a7ec2bcab51d92244e14dddd5200436203c98
Konrad Galczynski <kgalczynski@chathamfinancial.eu>
2018-07-13 17:58:15 -0400

    initial code commit

---
+++ CodingDojos/CodingDojos/FizzBuzzMain.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CodingDojos
+{
+    // requirements:
+    // - write a program that prints to the console the numbers from 1 to N where N is supplied by user.\
+    // - for multiples of three print “Fizz” instead of the number
+    // - for the multiples of five print “Buzz”
+    // - for numbers which are multiples of both three and five print “FizzBuzz”
+    internal class FizzBuzzMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz main");
+            Console.ReadKey();
+        }
+    }
+}
