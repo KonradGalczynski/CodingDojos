6431a2fe27c4518e58af82fa8f423eb141c01fc4
Konrad Gałczyński <konrad.galczynski@relativity.com>
2019-12-09 10:09:06 +0100

    revert to initial state

---
+++ CodingDojos/CodingDojos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodingDojos
+{
+	// requirements:
+	// - write a program that prints to the console the numbers from 1 to N where N is supplied by user.
+	// - for multiples of three print “Fizz” instead of the number
+	// - for the multiples of five print “Buzz”
+	// - for numbers which are multiples of both three and five print “FizzBuzz”
+
+	public class Program
+	{
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("Main Method");
+		}
+	}
+}
---
+++ CodingDojos/FizzBuzzSpecification/SampleSpecification.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzzSpecification
+{
+	using FluentAssertions;
+	using TddEbook.TddToolkit;
+	using Xunit;
+
+	public class SampleSpecification
+	{
+		[Fact]
+		public void Specification()
+		{
+			var a = Any.Integer();
+			var b = Any.Integer();
+
+			var expectedSum = a + b;
+
+			expectedSum.Should().Be(a + b);
+		}
+	}
+}
